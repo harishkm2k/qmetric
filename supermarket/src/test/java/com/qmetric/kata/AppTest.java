d74a4d3be3cdabcc38644f4a5d85e2163690145e
Harish.KamugakudiMarimuthu <Harish.Kamugakudi-Marimuthu@lloydsbanking.com>
2015-06-17 12:29:30 +0100

    TDD: simple test case to validate junit 4 and powermock is working.

--- supermarket/src/test/java/com/qmetric/kata/AppTest.java
+++ supermarket/src/test/java/com/qmetric/kata/AppTest.java
@@ -1,10 +1,9 @@
 
 package com.qmetric.kata;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -13,27 +12,12 @@ import org.powermock.modules.junit4.PowerMockRunner;
  * Unit test for simple App.
  */
 @RunWith(PowerMockRunner.class)
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     * 
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(
-            AppTest.class);
-    }
+public class AppTest {
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp() {
         assertTrue(true);
     }
