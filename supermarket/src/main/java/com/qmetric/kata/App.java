a2362196b3efd63239eac51cc1144d849456bee6
Harish.KamugakudiMarimuthu <Harish.Kamugakudi-Marimuthu@lloydsbanking.com>
2015-06-17 18:54:24 +0100

    SuperMarket - Kata with SOLID Principles :)

--- supermarket/src/main/java/com/qmetric/kata/App.java
+++ supermarket/src/main/java/com/qmetric/kata/App.java
@@ -1,12 +1,26 @@
 
 package com.qmetric.kata;
 
+import com.qmetric.kata.order.Order;
+import com.qmetric.kata.pay.Payment;
+
+
 /**
- * Hello world!
  * 
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    private Order order;
+
+    private Payment payment;
+
+    public void superMarketApp() {
+
+        // Compose the ItemPurchased object
+
+        // Compose the Order object
+
+        // Compose the Payment object
     }
+
 }
